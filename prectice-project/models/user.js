eafcb511958af82d42a22796c958cbe8ee526238
Hassaan Zahid <hassaan.zahid@invozone.com>
2022-05-27 19:38:42 +0500

    Working on invoices and menu module

--- prectice-project/models/user.js
+++ prectice-project/models/user.js
@@ -1,51 +1,51 @@
-const { Model } = require('sequelize')
-const Sequelize = require('sequelize')
+const { Model } = require('sequelize');
+const Sequelize = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
-    class User extends Model {
-        /**
+  class User extends Model {
+    /**
          * Helper method for defining associations.
          * This method is not a part of Sequelize lifecycle.
          * The `models/index` file will call this method automatically.
          */
-        static associate(models) {
-            // define association here
-        }
+    static associate(models) {
+      User.hasMany(models.Invoice, { as: 'invoices' });
     }
-    User.init(
-        {
-            firstName: {
-                type: DataTypes.STRING,
-                allowNull: false,
-            },
-            lastName: {
-                type: DataTypes.STRING,
-                allowNull: false,
-            },
-            email: {
-                type: Sequelize.STRING,
-                allowNull: false,
-                unique: true,
-                validate: {
-                    isEmail: {
-                        msg: 'Please provide valid email',
-                    },
-                },
-            },
-            type: {
-                type: Sequelize.ENUM('admin', 'client'),
-                allowNull: false,
-            },
-            phone: {
-                type: DataTypes.STRING,
-                allowNull: false,
-            },
-            password: DataTypes.STRING,
+  }
+  User.init(
+    {
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: {
+            msg: 'Please provide valid email',
+          },
         },
-        {
-            sequelize,
-            modelName: 'User',
-        }
-    )
-    return User
-}
+      },
+      type: {
+        type: Sequelize.ENUM('admin', 'client'),
+        allowNull: false,
+      },
+      phone: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: 'User',
+    },
+  );
+  return User;
+};
---
+++ prectice-project/seeders/20220527071046-demo-data.js
@@ -0,0 +1,39 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('PaymentVariations', [
+      {
+        type: 'Weekly',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        type: 'Monthly',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        type: 'Bi-Weekly',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+
+    await queryInterface.bulkInsert('Menuss', [
+      {
+        day: 'Monday',
+        item: 'Rice',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+         * Add commands to revert seed here.
+         *
+         * Example:
+         * await queryInterface.bulkDelete('People', null, {});
+         */
+  },
+};
